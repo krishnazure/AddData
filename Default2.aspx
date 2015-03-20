b897a8bc4f7cef7f4f95d8663df8fd56e0c7bbed
krishnazure <krishnaazure2014@gmail.com>
2015-03-19 23:27:33 -0400

    First Commit to GitHub

---
+++ Default2.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MyBootStrap.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat ="server">
+    <form id="form1" runat="server">
+    <br />
+    <br />
+    <p class="lead"><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+    </form>
+ </asp:Content>
+
---
+++ Default2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+        ISession session = cluster.Connect("demo");
+        session.Execute("insert into firstname(firstname) values ('" + TextBox1.Text + "')");
+
+    }
+}
